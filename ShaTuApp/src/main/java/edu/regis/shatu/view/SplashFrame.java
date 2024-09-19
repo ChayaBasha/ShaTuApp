eac7e709227c497f88269c91a5f475e238703109
Brent Krous <brent.krous@gmail.com>
2024-09-19 08:50:53 -0500

    [FIX] Invalid password attempt count (Jira Shat-110 issue)

--- ShaTuApp/src/main/java/edu/regis/shatu/view/SplashFrame.java
+++ ShaTuApp/src/main/java/edu/regis/shatu/view/SplashFrame.java
@@ -95,7 +95,7 @@ public class SplashFrame extends JFrame {
      * The number of consecutive illegal passwords attempted by the current
      * user attempting to login (see MAX_SIGNIN_ATTEMPTS).
      */
-    protected int signInAttempts = 0;
+    protected int signInAttempts = 1;
     
    /**
      * Create and layout the child components in this Spalsh JFrame.
@@ -147,7 +147,7 @@ public class SplashFrame extends JFrame {
      * of the number of user attempts thus far.     * 
      */
     public void invalidPass() {
-        if (signInAttempts < MAX_SIGNIN_ATTEMPTS) {
+        if (signInAttempts <= MAX_SIGNIN_ATTEMPTS) {
            
             String msg = "Invalid Password attempt " + 
                          String.valueOf(signInAttempts) + " of " + 
